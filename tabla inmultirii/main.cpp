a1da9c4d2a3f177d1e79af0824669836c4a93b56
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2019-10-14 22:02:41 +0300

    Numararea cifrelor dintr-un nr

---
+++ tabla inmultirii/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+      {
+   int i;
+   int j;
+   for (i = 1; i<=25; i++){
+        for ( j = 1; j <=25; j++)
+                cout << i * j <<" ";
+                cout<<"\n";
+        }
+      }
+    return 0;
+}
