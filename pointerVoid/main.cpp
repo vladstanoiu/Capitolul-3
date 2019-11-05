b126e2465222029f01180061907f08b7daa32a5d
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2019-11-05 18:36:29 +0200

    Update main.cpp

--- pointerVoid/main.cpp
+++ pointerVoid/main.cpp
@@ -7,9 +7,9 @@ int main()
     int vlad;
     void* pntVlad;
     pntVlad = &vlad;
-    static_cast <int*>(pntVlad);
+    *static_cast <int*>(pntVlad)= 60;
 
-    cout << "Variabila este: " << pntVlad << endl;
-    cout << "Variabila prin Void este: " << static_cast<int*>(pntVlad) << endl;
+    cout << "Variabila este: " << vlad << endl;
+    cout << "Variabila prin Void este: " << *static_cast<int*>(pntVlad) << endl;
     return 0;
 }
